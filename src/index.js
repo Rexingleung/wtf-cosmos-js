458203deb69d78946a856ac236d373b6f7e8fef7
leung <rexingleung@126.com>
2025-08-21 07:15:18 +0800

    Update src/index.js to export all modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
-const {createServer} = require('./server.js')
+/**
+ * WTF Cosmos JS - Source Index
+ * 源码模块导出
+ */
+
+const { Blockchain, Block, Transaction } = require('./blockchain');
+const { Wallet, KeyManager } = require('./crypto');
+const { ProofOfWork, ValidatorManager } = require('./consensus');
+const { GovernanceManager, Proposal, VoteOption } = require('./governance');
+const { createServer, startServer } = require('./server');
 const config = require('./config');
+const { logger, helpers } = require('./utils');
 
-const server = createServer();
-
-server.listen(config.PORT, () => {
-	console.log('listen ' + config.PORT);
-	
-})
+module.exports = {
+  // 区块链核心
+  Blockchain,
+  Block,
+  Transaction,
+  
+  // 加密模块
+  Wallet,
+  KeyManager,
+  
+  // 共识机制
+  ProofOfWork,
+  ValidatorManager,
+  
+  // 治理模块
+  GovernanceManager,
+  Proposal,
+  VoteOption,
+  
+  // 服务器
+  createServer,
+  startServer,
+  
+  // 配置和工具
+  config,
+  logger,
+  helpers
+};
